7f96c4486a73fc9772ef6e11f7b72cb0119c7811
fsclass-n <whyeil@naver.com>
2026-03-21 16:53:13 +0900

    gh

---
+++ basic/ch01/ex05_파이썬둘러보기.py
@@ -0,0 +1,36 @@
+# 사칙 연산
+var = 1+2
+print(var)
+var = 3 / 2.4
+print(var)
+var = 3 * 9
+print(var)
+
+# 변수
+a = 1
+b = 2
+print(a + b)
+
+# 문자열
+a = "Python"
+print(a)
+
+# 조건문 if
+a = 3
+if a > 1:
+    print("a는 1보 큽니다.")
+
+# 반복문 for
+for a in [1, 2, 3]:
+    print(a)
+
+# 반복문 while
+i = 0
+while i < 3:
+    i = i + 1
+    print(i)
+
+# 함수
+def add(a, b):
+    return a + b
+print(add(3, 4))
